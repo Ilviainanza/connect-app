caec6eb37bc727ea6c28072606eaf6dfe634c160
Ilviainanza <ilviainanza3@gmail.com>
2025-05-28 13:10:07 +0800

    components and looping

---
+++ src/mock/data-user.js
@@ -0,0 +1,14 @@
+export const dataUsers = [
+    {
+        fullname: "Rahmad Saudi Al Fathir",
+        email: "rahmadsaudi@universitasmulia.ac.id",
+        role: "Admin",
+        status: "Aktif",
+    },
+    {
+        fullname: "Shafira",
+        email: "Shafira@universitasmulia.ac.id",
+        role: "Employee",
+        status: "Suspended", 
+    },
+];
